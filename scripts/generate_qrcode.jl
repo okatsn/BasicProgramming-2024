53a7db893d2a32fc902e7a66ae3c87cc9d5bb37c
okatsn <okatsn@gmail.com>
2024-11-11 08:58:14 +0000

    Quiz link with test id number

--- scripts/generate_qrcode.jl
+++ scripts/generate_qrcode.jl
@@ -9,9 +9,7 @@ student_information = CSV.read("student_information.csv", DataFrame)
 
 quiz_link(score_A, score_B, violation_num, note, my_name, stud_ID, test_name) = "$(form_quiz)?usp=pp_url&entry.482342201=$(score_A)&entry.1809338332=$(score_B)&entry.2125071475=$(violation_num)&entry.1825598537=$(note)&entry.1529579107=$(my_name)&entry.838829525=$(stud_ID)&entry.658137221=$(test_name)"
 
-quiz_link(my_name, stud_ID, test_name) = "$(form_quiz)?usp=pp_url&entry.1529579107=$(my_name)&entry.838829525=$(stud_ID)&entry.658137221=$(test_name)"
-
-
+quiz_link(my_name, stud_ID, test_name, quiznum_A, quiznum_B) = "$(form_quiz)?usp=pp_url&entry.1529579107=$(my_name)&entry.838829525=$(stud_ID)&entry.658137221=$(test_name)&entry.1632268405=$(quiznum_A)&entry.1132409593=$(quiznum_B)"
 
 
 # # Generate QRCode
