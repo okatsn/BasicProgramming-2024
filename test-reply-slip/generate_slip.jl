5702e613d53ff635d664a146587a30e10acd310a
okatsn <okatsn@gmail.com>
2024-11-11 05:07:16 +0000

    Quiz link and DVC the secrets

--- test-reply-slip/generate_slip.jl
+++ test-reply-slip/generate_slip.jl
@@ -1,2 +1,7 @@
 using JSON, OkReadGSheet, DataFrames, CSV, QRCoders, Chain
 using OkReadGSheet
+
+
+form_quiz # please load from secrets.json
+
+quiz_link(score_A, score_B, violation_num, note, my_name, stud_ID, test_name) = "$(form_quiz)/viewform?usp=pp_url&entry.482342201=$(score_A)&entry.1809338332=$(score_B)&entry.2125071475=$(violation_num)&entry.1825598537=$(note)&entry.1529579107=$(my_name)&entry.838829525=$(stud_ID)&entry.658137221=$(test_name)"
