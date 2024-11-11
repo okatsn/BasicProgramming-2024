073a49113134f0d4be32382dd0bc45f30902f6e2
okatsn <okatsn@gmail.com>
2024-11-11 07:57:35 +0000

    Preprocess score_quiz

--- test-reply-slip/generate_slip.jl
+++ test-reply-slip/generate_slip.jl
@@ -1,9 +1,31 @@
 using JSON, OkReadGSheet, DataFrames, CSV, QRCoders, Chain, SMTPClient
+using Dates
 using OkReadGSheet
 
 do_send_email = false
 
 secrets = JSON.parsefile("local/secrets.json")
+
+score_quiz = @chain readgsheet(secrets["score_quiz"]) begin
+    transform!(
+        "測驗名稱" => :Test,
+        "學號" => :StudentID,
+        "姓名" => :Name,
+        "測驗A" => :Score_A,
+        "測驗B" => :Score_B,
+        "電子郵件地址" => :MyEmail,
+        "違規註記" => :Violate,
+        "備註" => :Note,
+        "時間戳記" => :Time
+    )
+    transform!(:Time => ByRow(s -> replace(s, "上午" => "AM")); renamecols=false)
+    transform!(:Time => ByRow(s -> replace(s, "下午" => "PM")); renamecols=false)
+    transform!(:Time => ByRow(t -> DateTime(t, dateformat"yyyy/mm/dd p II:MM:SS")); renamecols=false)
+    select(Not(r"[\u4e00-\u9fff]")) # remove all the columns with ZH characters
+    # filter!()
+end
+
+
 
 form_quiz = secrets["form_quiz"] # please load from secrets.json
 sender_key = secrets["sender_key"]
