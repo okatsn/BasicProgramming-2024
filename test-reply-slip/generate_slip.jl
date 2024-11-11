7b3c207cfc7d09406b42407e7d3eee13c2635dd0
okatsn <okatsn@gmail.com>
2024-11-11 06:26:51 +0000

    Fix quiz_link and generate prefilled links.

--- test-reply-slip/generate_slip.jl
+++ test-reply-slip/generate_slip.jl
@@ -8,4 +8,12 @@ sender_key = secrets["sender_key"]
 
 student_table = CSV.read("student_information.csv", DataFrame)
 
-quiz_link(score_A, score_B, violation_num, note, my_name, stud_ID, test_name) = "$(form_quiz)/viewform?usp=pp_url&entry.482342201=$(score_A)&entry.1809338332=$(score_B)&entry.2125071475=$(violation_num)&entry.1825598537=$(note)&entry.1529579107=$(my_name)&entry.838829525=$(stud_ID)&entry.658137221=$(test_name)"
+quiz_link(score_A, score_B, violation_num, note, my_name, stud_ID, test_name) = "$(form_quiz)?usp=pp_url&entry.482342201=$(score_A)&entry.1809338332=$(score_B)&entry.2125071475=$(violation_num)&entry.1825598537=$(note)&entry.1529579107=$(my_name)&entry.838829525=$(stud_ID)&entry.658137221=$(test_name)"
+
+quiz_link(my_name, stud_ID, test_name) = "$(form_quiz)?usp=pp_url&entry.1529579107=$(my_name)&entry.838829525=$(stud_ID)&entry.658137221=$(test_name)"
+
+
+this_test = "11/18 上機測試 (範圍 1-4)"
+
+# row = eachrow(student_table)[2]
+links = [quiz_link(row.Name, row.StudentID, this_test) for row in eachrow(student_table)]
