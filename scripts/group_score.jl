ebb9600a2618d7708f777f3826a5ca194704d49d
okatsn <okatsn@gmail.com>
2024-11-12 07:18:34 +0000

    Keynotes on gorup scores.

---
+++ scripts/group_score.jl
@@ -0,0 +1,24 @@
+# # This is the script for group presentation scores.
+# General
+# - A-D (total four group)
+# - 12/16 explain in class. (all should be prepared).
+#
+# (1) `A` scores other groups (`B`, `C`, `D`):
+# - 5% (absolute score 1 to 5)
+# - 12/23 `A` and `B` present
+# - 12/30 `C` and `D` present
+#
+# (2) Member `one` scores others:
+# - 10% (absolute score 1 to 10)
+#
+#
+# Shortnote:
+# - (1): Three QRCodes for each student (for the other three groups), fill into Google form (a).
+# - (2): Three QRCodes for each student (for the other 3 team members), fill into Google form (b).
+# - (a): 評分者、被評組別、分數
+# - (b): 評分者、被評組員、分數
+#
+#
+# Verification:
+# - (a): one 評分者 should have exactly three answers where 被評組別 is the other three.
+# - (b): one 評分者 should have exactly three answers where 被評組員 is the other three.
