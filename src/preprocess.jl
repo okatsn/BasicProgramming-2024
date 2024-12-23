fff2113f65143854c67dcf40a2622f1ac78d4f6c
okatsn <okatsn@gmail.com>
2024-12-23 09:03:33 +0000

    convdatetime!

--- src/preprocess.jl
+++ src/preprocess.jl
@@ -1,13 +1,15 @@
-
+convdatetime!(df) = @chain df begin
+    transform!(:Time => ByRow(s -> replace(s, "上午" => "AM")); renamecols=false)
+    transform!(:Time => ByRow(s -> replace(s, "下午" => "PM")); renamecols=false)
+    transform!(:Time => ByRow(t -> DateTime(t, dateformat"yyyy/mm/dd p II:MM:SS")); renamecols=false)
+end
 
 function quizscoreprep!(df)
     @chain df begin
         transform!(
             gformheaders_quizscore...
         )
-        transform!(:Time => ByRow(s -> replace(s, "上午" => "AM")); renamecols=false)
-        transform!(:Time => ByRow(s -> replace(s, "下午" => "PM")); renamecols=false)
-        transform!(:Time => ByRow(t -> DateTime(t, dateformat"yyyy/mm/dd p II:MM:SS")); renamecols=false)
+        convdatetime!
         select!(Not(r"[\u4e00-\u9fff]")) # remove all the columns with ZH characters
     end
 end
