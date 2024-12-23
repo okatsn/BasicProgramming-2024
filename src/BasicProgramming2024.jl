2281f8781977fa73e9e07292b94322a556f4ff06
okatsn <okatsn@gmail.com>
2024-12-23 09:15:14 +0000

    Fix header pairs.

--- src/BasicProgramming2024.jl
+++ src/BasicProgramming2024.jl
@@ -36,22 +36,22 @@ const gformheaders_quizscore = (
 )
 
 const gformheaders_innergroupscore = (
-    "時間戳記" => ":Time",
-    "電子郵件地址" => ":TheirEmail",
-    "我的組員1" => "Member_1",
-    "組員1的表現" => "Score_1",
-    "我的組員2" => "Member_2",
-    "組員2的表現" => "Score_2",
-    "我的組員3" => "Member_3",
-    "組員3的表現" => "Score_3",
-    "附註" => ":Note",
+    "時間戳記" => :Time,
+    "電子郵件地址" => :TheirEmail,
+    "我的組員1" => :Member_1,
+    "組員1的表現" => :Score_1,
+    "我的組員2" => :Member_2,
+    "組員2的表現" => :Score_2,
+    "我的組員3" => :Member_3,
+    "組員3的表現" => :Score_3,
+    "附註" => :Note,
 )
 
 const gformheaders_intergroupscore = (
-    "時間戳記" => ":Time",
-    "電子郵件地址" => ":TheirEmail",
-    "報告組別" => "Group",
-    "報告與內容綜合表現" => "Score",
-    "附註" => ":Note",
+    "時間戳記" => :Time,
+    "電子郵件地址" => :TheirEmail,
+    "報告組別" => :Group,
+    "報告與內容綜合表現" => :Score,
+    "附註" => :Note,
 )
 end
