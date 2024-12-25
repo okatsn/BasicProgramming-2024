ce593c280fefd36f76dddf70ca9e874e7a19d192
okatsn <okatsn@gmail.com>
2024-12-25 04:29:31 +0000

    Revise takelast.

--- scripts/update_quiz_score.jl
+++ scripts/update_quiz_score.jl
@@ -13,6 +13,7 @@ score_quiz = @chain readgsheet(secrets["score_quiz"]) begin
     quizscoreprep!
     filter!(:MyEmail => x -> (x in secrets["filler"]), _) # verify who fill the form.
     filter!(keepthistest, _)
+    groupby([:Name, :StudentID])
     takelast
 end
 
--- src/takelast.jl
+++ src/takelast.jl
@@ -1,6 +1,8 @@
-function takelast(df)
-    @chain df begin
-        groupby([:Name, :StudentID])
+"""
+Combine groupDataframe `gdf` taking only the row in each `gdf` that has the largest `:Time`.
+"""
+function takelast(gdf)
+    @chain gdf begin
         combine(gd -> gd[argmax(gd.Time), :]) # take the row with the largest time.
     end
 end
