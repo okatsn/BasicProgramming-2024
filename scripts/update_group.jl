d90ca83707bc8fbd6fbcd27559967638b6ba0029
okatsn <okatsn@gmail.com>
2024-12-25 04:02:35 +0000

    Warning on authentication results.

--- scripts/update_group.jl
+++ scripts/update_group.jl
@@ -10,7 +10,8 @@ student_information = CSV.read("student_information.csv", DataFrame)
 gmail_name = Dict(student_information.gmail .=> student_information.Name)
 gmail_gid = Dict(student_information.gmail .=> student_information.GroupID)
 
-inner_score = @chain readgsheet(secrets["score_innerGroup"]) begin
+inner_score0 = readgsheet(secrets["score_innerGroup"])
+inner_score = @chain inner_score0 begin
     innerscoreprep!
     transform!(Cols(r"Member_\d+") => ByRow((args...) -> Set(args)) => :NotMe)
 
@@ -18,11 +19,20 @@ inner_score = @chain readgsheet(secrets["score_innerGroup"]) begin
     filter!(:TheirEmail => in(student_information.gmail), _) # remove alien filler
     filter!(AsTable(:) => nt -> !in(gmail_name[nt.TheirEmail], nt.NotMe), _) # Filler cannot score him/herself
 end
-inter_score = @chain readgsheet(secrets["score_interGroup"]) begin
+
+inter_score0 = readgsheet(secrets["score_interGroup"])
+inter_score = @chain inter_score0 begin
     interscoreprep!
     transform!(:TheirEmail => ByRow(x -> gmail_gid[x]) => :GID)
 
     # Authentication
     filter!(:TheirEmail => in(student_information.gmail), _) # remove alien filler
     filter!(AsTable(:) => (nt -> !isequal(nt.GID, nt.Group) && length(nt.Group) == 1), _) # Filler's group (GID) cannot be the score's group; additional check of nt.Group's string length.
+end
+
+if nrow(inner_score) != nrow(inner_score0)
+    @warn "Some reply do not pass authentication (Inner score)."
+end
+if nrow(inter_score) != nrow(inter_score0)
+    @warn "Some reply do not pass authentication (Inner score)."
 end
