9e2db3ad2453f43954eace7cb9ec565ff92920ef
okatsn <okatsn@gmail.com>
2024-12-25 06:18:00 +0000

    Add a stage `send_group_score`

---
+++ scripts/send_group_score.jl
@@ -0,0 +1,8 @@
+using DataFrames, CSV
+
+inner = @chain CSV.read("data/intergroup_score.csv", DataFrame) begin
+    transform(:who_did_not_score => ByRow(x -> eval(Meta.parse(x))); renamecols=false)
+end
+inter = @chain CSV.read("data/innergroup_score.csv", DataFrame) begin
+    transform(:who_did_not_score => ByRow(x -> eval(Meta.parse(x))); renamecols=false)
+end
