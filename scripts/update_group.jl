5931742132fe0bf0621852d6079ae29733c2e0dd
okatsn <okatsn@gmail.com>
2024-12-25 05:19:40 +0000

    Calculate NotMe at the level of student_information

--- scripts/update_group.jl
+++ scripts/update_group.jl
@@ -14,7 +14,14 @@ end
 
 gid_gmails = gid_gmails |> (x -> Dict(x.GroupID .=> x.gmails))
 
+@chain student_information begin
+    transform!(:GroupID => ByRow(i -> Set(gid_gmails[i])) => :AllMembers)
+    transform!([:gmail, :AllMembers] => ByRow((r, a) -> Set(setdiff(a, [r]))) => :NotMe) # noted that the argument order in setdiff is critical.
+end
+
+gmail_notme = Dict(student_information.gmail .=> student_information.NotMe)
 gmail_name = Dict(student_information.gmail .=> student_information.Name)
+name_gmail = Dict(student_information.Name .=> student_information.gmail)
 gmail_gid = Dict(student_information.gmail .=> student_information.GroupID)
 
 # # Authenticate
@@ -23,8 +30,7 @@ inner_score0 = readgsheet(secrets["score_innerGroup"])
 inner_score = @chain inner_score0 begin
     innerscoreprep!
     transform!(:ReplierEmail => ByRow(x -> gmail_gid[x]) => :GID)
-    transform!(:GID => ByRow(i -> gid_gmails[i]) => :AllMembers)
-    transform!([:ReplierEmail, :AllMembers] => ByRow((r, a) -> Set(setdiff(a, [r]))) => :NotMe) # noted that the argument order in setdiff is critical.
+    transform!(:ReplierEmail => ByRow(i -> gmail_notme[i]) => :NotMe)
 
     # Authentication
     filter!(:ReplierEmail => in(student_information.gmail), _) # remove alien replier
