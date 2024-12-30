f56439ff1a41df56f5cb030495396a1460fc908b
okatsn <okatsn@gmail.com>
2024-12-30 09:25:52 +0000

    Select header before CSV.write.

--- scripts/update_quiz_score.jl
+++ scripts/update_quiz_score.jl
@@ -9,6 +9,8 @@ keepthistest = :Test => x -> (x .== this_test)
 
 secrets = JSON.parsefile("local/secrets.json")
 
+header0 = [:Test, :StudentID, :Name, :Score_A, :QuizNum_A, :Score_B, :QuizNum_B, :MyEmail, :Violate, :Note, :Time]
+
 score_quiz = @chain readgsheet(secrets["score_quiz"]) begin
     quizscoreprep!
     filter!(:MyEmail => x -> (x in secrets["filler"]), _) # verify who fill the form.
@@ -17,15 +19,18 @@ score_quiz = @chain readgsheet(secrets["score_quiz"]) begin
     takelast
 end
 
+
 if isfile("data/quiz_score.csv")
     header = false
     app = true
 else
-    header = [:Test, :StudentID, :Name, :Score_A, :QuizNum_A, :Score_B, :QuizNum_B, :MyEmail, :Violate, :Note, :Time]
+    header = header0
     app = false
 end
 
-CSV.write("data/quiz_score.csv", score_quiz, header=header, append=app)
+CSV.write("data/quiz_score.csv",
+    select(score_quiz, header0), # Select is critical to assure the appended rows corresponds the the correct column header.
+    header=header, append=app)
 # KEYNOTE: You need to set `persist: true` in dvc.yaml; otherwise, the output will be removed before `dvc repro`.
 # E.g.
 # ```dvc.yaml
