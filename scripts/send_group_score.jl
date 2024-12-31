3d3788909548946a46de07e337a282083bbf608c
okatsn <okatsn@gmail.com>
2024-12-31 06:08:25 +0000

    Functions for getting notes.

--- scripts/send_group_score.jl
+++ scripts/send_group_score.jl
@@ -4,18 +4,27 @@ using OkReadGSheet
 using BasicProgramming2024
 using Statistics
 
-secrets = JSON.parsefile("local/secrets.json")
+do_send_email = false
+student_information = CSV.read("student_information.csv", DataFrame)
 
-df = @chain CSV.read("data/innergroup_with_note.csv", DataFrame) begin
+note_inner = @chain CSV.read("data/innergroup_with_note.csv", DataFrame) begin
     dropmissing(:Note)
     groupby(:Name)
-    combine(:Note => (x -> join(x, "\n- ")))
+    combine(:Note => (x -> join(x, "\n- ")) => :note_summary)
 end
+innernote(name) = Dict(note_inner.Name .=> note_inner.note_summary)[name]
 
 
-
-df2 = @chain CSV.read("data/intergroup_with_note.csv", DataFrame) begin
+note_inter = @chain CSV.read("data/intergroup_with_note.csv", DataFrame) begin
     dropmissing(:Note)
     groupby(:Group)
-    combine(:Note => (x -> join(x, "\n- ")))
+    combine(:Note => (x -> join(x, "\n- ")) => :note_summary)
 end
+internote(gid) = Dict(note_inter.Group .=> note_inter.note_summary)[gid]
+
+
+score_inter = CSV.read("data/intergroup_score.csv", DataFrame)
+interscore(gid) = Dict(score_inter.GroupID .=> score_inter.score_mean)[gid]
+
+score_inner = CSV.read("data/innergroup_score.csv", DataFrame)
+innerscore(name) = Dict(score_inner.Name .=> score_inner.score_mean)[name]
